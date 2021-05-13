1b66c3af21e2524708bfac43d9c0fbf68deb75dc
AlexUioreanu <83290173+AlexUioreanu@users.noreply.github.com>
2021-05-13 16:50:03 +0300

    Update SalesRepresentative.java

--- SalesRepresentative.java
+++ SalesRepresentative.java
@@ -3,12 +3,12 @@ package TemaFunctions1AlexUio;
 public class SalesRepresentative extends Algo {
     private int sale;
     private int quota;
-    private int revenue = sale * quota;
+    private int revenue;
 
-    public SalesRepresentative(int sale, int quota, int revenue) {
+    public SalesRepresentative(int sale, int quota) {
         this.sale = sale;
         this.quota = quota;
-        this.revenue = revenue;
+        this.revenue = quota*sale;
     }
 
     public int getRevenue() {
