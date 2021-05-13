c334d5bd0d45adf8082fff5a715feecc14c3594a
AlexUioreanu <83290173+AlexUioreanu@users.noreply.github.com>
2021-05-13 16:51:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,24 @@ import java.util.Arrays;
 public class Main extends Algo {
     public static void main(String[] args) {
 
-        SalesRepresentative guy1 = new SalesRepresentative(10, 700, 7000);
-        SalesRepresentative guy2 = new SalesRepresentative(7, 800, 5600);
-        SalesRepresentative guy3 = new SalesRepresentative(8, 600, 4800);
-        SalesRepresentative guy4 = new SalesRepresentative(10, 1000, 10000);
-        SalesRepresentative guy5 = new SalesRepresentative(10, 800, 8000);
-        SalesRepresentative[] reps = new SalesRepresentative[]{guy1, guy2, guy3, guy4, guy5};
+        SalesRepresentative[] reps = getSalesRepresentatives();
 
         Algo sort = new Algo();
 
         SalesRepresentative[] sortedReps = sort.sort(reps);
 
         System.out.println(Arrays.toString(sortedReps));
+    }
+    
+    public static SalesRepresentative[] getSalesRepresentatives() {
+        SalesRepresentative guy1 = new SalesRepresentative(10, 700);
+        SalesRepresentative guy2 = new SalesRepresentative(7, 800);
+        SalesRepresentative guy3 = new SalesRepresentative(8, 600);
+        SalesRepresentative guy4 = new SalesRepresentative(10, 1000);
+        SalesRepresentative guy5 = new SalesRepresentative(10, 800);
+
+        SalesRepresentative[] reps = new SalesRepresentative[]{guy1, guy2,guy3,guy4,guy5};
+        return reps;
     }
 }
 
