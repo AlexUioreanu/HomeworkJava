658babfaf8c47ea8e403269f896889e2191ccbd8
AlexUioreanu <83290173+AlexUioreanu@users.noreply.github.com>
2021-05-18 09:46:34 +0300

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -2,6 +2,9 @@ package TemaOOPAlexUio2;
 
 import org.apache.commons.lang3.ArrayUtils;
 
+/**
+ * Class that contains the the contact with the list of messages and calls
+ */
 public class Contact {
     private int id;
     private int phoneNumber;
@@ -10,6 +13,13 @@ public class Contact {
     private Message[] listmessage;
     private Call[] callHistory;
 
+    /**
+     * Constructor that contains even the list of messages and calls
+     * @param id contains the id of the contact
+     * @param phoneNumber contains the phone number of the contact
+     * @param firstName contains the first name of the contact
+     * @param lastName contains the last name of the contact
+     */
     public Contact(int id, int phoneNumber, String firstName, String lastName) {
         this.id = id;
         this.phoneNumber = phoneNumber;
