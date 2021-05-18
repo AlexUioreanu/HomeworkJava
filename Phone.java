df2f2ad21704a974705e6b8253435adec5a5bd99
AlexUioreanu <83290173+AlexUioreanu@users.noreply.github.com>
2021-05-18 09:54:58 +0300

    Update Phone.java

--- Phone.java
+++ Phone.java
@@ -1,6 +1,14 @@
 package TemaOOPAlexUio2;
 
 public interface Phone {
+    /**
+     * Interface that contains all the phone behaviors
+     *
+     * @param id          contains the id of the contact
+     * @param phoneNumber contains the phone number of the contact
+     * @param firstName   contains the first name of the contact
+     * @param lastName    contains the last name of the contact
+     */
     void addContact(int id, int phoneNumber, String firstName, String lastName);
     void listContacts();
     void sendMessage(int phoneNumber, String msg);
