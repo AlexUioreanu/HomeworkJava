a679fae300e7af7df2ad16c5b2707e73de61626a
AlexUioreanu <83290173+AlexUioreanu@users.noreply.github.com>
2021-06-12 12:12:53 +0300

    Delete Student.java

--- Student.java
+++
@@ -1,9 +0,0 @@
-package TemaObjContainersAlexUio;
-
-public class Student extends Persoana{
-    private boolean cautaDeLucru;
-
-    public Student(String name,int age) {
-        super(name, age);
-    }
-}
