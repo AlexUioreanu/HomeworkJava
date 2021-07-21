f759548cf901b2219c0c1b3b893e9ef0bbc64fb2
AlexUioreanu <83290173+AlexUioreanu@users.noreply.github.com>
2021-07-21 14:19:25 +0300

    Update TicketType.java

--- ConcurrencyHW/src/main/java/TicketType.java
+++ ConcurrencyHW/src/main/java/TicketType.java
@@ -11,13 +11,4 @@ public enum TicketType {
         int pick = new Random().nextInt(TicketType.values().length);
         return TicketType.values()[pick];
     }
-
-//    private static final List<TicketType> VALUES =
-//            List.of(values());
-//    private static final int SIZE = VALUES.size();
-//    private static final Random RANDOM = new Random();
-//
-//    public static TicketType randomTicket()  {
-//        return VALUES.get(RANDOM.nextInt(SIZE));
-//    }
 }
